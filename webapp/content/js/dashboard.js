3ff6fdf540dd7398b393431635394b2c6d5d4ccd
Christopher Bowman <cbowman0@gmail.com>
2012-08-24 13:54:08 +0000

    Allow the creation of a graph from a metric.  This is useful when you have a function based metric that isn't currently part of a URL or saved graph.

--- webapp/content/js/dashboard.js
+++ webapp/content/js/dashboard.js
@@ -570,6 +570,9 @@ function initDashboard () {
               { text: "From Saved Graph",
                 handler: newFromSavedGraph
               },
+              { text: "From Metric",
+                handler: newFromMetric
+              },
             ]
           }
         },
@@ -1431,6 +1434,70 @@ function newFromSavedGraph() {
           selectUserGraphs(Ext.getCmp('user-graphs-tree').getSelectionModel().getSelectedNodes());
           win.close();
         }
+      }, {
+        text: 'Cancel',
+        handler: function () { win.close(); }
+      }
+    ]
+  });
+  win.show();
+}
+
+function newFromMetric() {
+  function applyMetric() {
+    var inputMetric = Ext.getCmp('import-metric-field').getValue();
+    if (inputMetric == "") {
+      return;
+    }
+    var graphTargetString = Ext.urlEncode({target: inputMetric});
+
+    var myParams = {
+      target: [inputMetric]
+    };
+
+    var urlParams = {};
+    Ext.apply(urlParams, defaultGraphParams);
+    Ext.apply(urlParams, myParams);
+    Ext.apply(urlParams, GraphSize);
+
+    var record = new GraphRecord({
+      target: graphTargetString,
+      params: myParams,
+      url: '/render?' + Ext.urlEncode(urlParams)
+      });
+    graphStore.add([record]);
+    updateGraphRecords();
+    win.close();
+  }
+
+  var urlField = new Ext.form.TextField({
+    id: 'import-metric-field',
+    fieldLabel: "Metric",
+    region: 'center',
+    width: '100%',
+    listeners: {
+      specialkey: function (field, e) {
+                    if (e.getKey() == e.ENTER) {
+                      applyUrl();
+                    }
+                  },
+      afterrender: function (field) { field.focus(false, 100); }
+    }
+  });
+
+  var win = new Ext.Window({
+    title: "Import Graph From Metric",
+    width: 470,
+    height: 87,
+    layout: 'form',
+    resizable: true,
+    modal: true,
+    items: [urlField],
+    buttonAlign: 'center',
+    buttons: [
+      {
+        text: 'OK',
+        handler: applyMetric
       }, {
         text: 'Cancel',
         handler: function () { win.close(); }
